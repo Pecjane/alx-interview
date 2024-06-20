fff72741eb35a0e76cf7b2fab53d99edcf584fbe
Pecjane <promisebarth@gmail.com>
2024-06-20 20:37:16 +0000

    generator stat

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 """
-Log parsing
+Reads stdin line by line and computes metrics.
 """
-
 import sys
+import re
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+def print_data(fSize, statusCodes):
+    """
+    Prints the data after every 10 lines or keyboard interrupt.
+    """
+    print(f"File size: {fSize}")
+    for key, value in statusCodes.items():
+        if value != 0:
+            print(f"{key}: {value}")
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
 
+if __name__ == "__main__":
+    lines = 0
+    total_file_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    regex1 = r'(\d+.){3}\d+\s?-\s?\[(\d{4}-\d{2}-\d{2} '
+    regex2 = r'\d{2}:\d{2}:\d{2}.\d{6})\] '
+    regex3 = r'"GET /projects/260 HTTP/1.1" \d{3} \d+'
+    regex = regex1 + regex2 + regex3
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+            line = line.rstrip('\n')
+            if not re.search(regex, line):
+                continue
+            lines += 1
+            split_line = line.split(' ')
+            file_size = int(split_line[-1])
+            status_code = split_line[-2]
+            total_file_size += file_size
+            status_codes[status_code] += 1
+            if lines % 10 == 0:
+                print_data(total_file_size, status_codes)
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+        print_data(total_file_size, status_codes)
+    finally:
+        print_data(total_file_size, status_codes)
