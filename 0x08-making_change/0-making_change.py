bf9050916a8856d0939ddae523f7bf8189aeb6f2
Pecjane <promisebarth@gmail.com>
2024-07-26 05:00:55 +0000

    making

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
+
 """
-The change making challenge using dynamic programming.
+Module for Making change
 """
 
 
 def makeChange(coins, total):
     """
-    Returns the minimum amount of coins needed to meet total.
-    Steps:
-        - Get the least coins starting from the least amount
-        with the value of least coin.
-        - Amount - coin = newAmount
-        - is newAmount in cachedData if not then it's the 1st.
-        - else least coins of newAmount plus one.
-        - Do that for every coin and get the least.
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
-    # Bottom up approach.
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    if len(coins) == 0:
-        return -1
-    if total < min(coins):
-        return -1
-    minCoinsPerTotal = {}
-    for amount in range(min(coins), total+1):
-        minCoinsPerTotal[amount] = total+1
-        for coin in coins:
-            newAmount = amount - coin
-            if newAmount < 0:
-                continue
-            if newAmount not in minCoinsPerTotal and newAmount == 0:
-                minCoinsPerTotal[amount] = 1
-            elif newAmount not in minCoinsPerTotal and newAmount > 0:
-                pass
-            elif (minCoinsPerTotal[newAmount] + 1) < minCoinsPerTotal[amount]:
-                minCoinsPerTotal[amount] = minCoinsPerTotal[newAmount] + 1
-    res = -1 if minCoinsPerTotal[total] >= total+1 else minCoinsPerTotal[total]
-    return res
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
